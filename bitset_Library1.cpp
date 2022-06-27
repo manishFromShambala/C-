62292ff44bd7d599ed5c50431db94118ed773b7c
ROUTHU MANISH SRI SAI SURYA <108282274+manishFromShambala@users.noreply.github.com>
2022-06-27 20:00:11 +0530

    bitset_Library1.cpp

---
+++ bitset_Library1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define M 200
+int main(){
+	
+	int n=0,i,j;
+	cin>>n;string s;
+	cin>>s;
+	bitset<M> b(string());
+	//cout<<b;
+	/*for( i=0;i<M;i++){
+		if(b[i]==1){
+			break;
+		}
+	}*/
+	//for(j=i;j<M;j++)
+	cout<<b;
+	
+	return 0;
+}
